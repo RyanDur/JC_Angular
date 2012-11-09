3aa35549456632c63b7969320e8adc111445bc54
RyanDurling <ryan.p.durling@gmail.com>
2012-11-08 17:40:57 -0800

    adding mistakes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,13 +3,6 @@
 /* Controllers */
 
 
-function VolunteersCtrl($scope, $http) {
-    var volunteer_url = "/app/volunteers.json"
-    function init() {
-        $http({method:'GET', url:volunteer_url}).success(function(data){
-            $scope.volunteers = data;
-        });
-    }
-    
-    init();
+function VolunteersCtrl($scope, Volunteer) {
+    $scope.volunteers = Volunteer.query();
 }
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,11 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('volunteerServices', ['ngResource']).
+    factory('Volunteer', function($resource){
+   return $resource('app/:volunteers.json', {}, {
+       query: {method:'GET', isArray:true}
+});
+});
+
+
